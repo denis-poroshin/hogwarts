2dbf5bed1f2b7cdd3584c9e32b330888a664b1b2
PC <git@github.com:denis-poroshin/store1.git>
2024-08-17 14:02:41 +0300

    Начало

---
+++ src/main/java/ru/hogwarts/springhogwars/SpringHogwarsApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.springhogwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHogwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHogwarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwarts/springhogwars/SpringHogwarsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.springhogwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHogwarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
